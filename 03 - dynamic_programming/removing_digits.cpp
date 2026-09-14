7e70541c79bd9b638ecb692fc6aecbbf2e6469a7
Kevin Zhang <peachedpeachypeaches@gmail.com>
2026-09-14 11:10:43 -0400

    day 6

--- 03 - dynamic_programming/removing_digits.cpp
+++ 03 - dynamic_programming/removing_digits.cpp
@@ -3,13 +3,49 @@
 using namespace std;
 #define ll long long
 
-int main() {
-    ll n; cin >> n;
-    vector<ll> answers(n + 1, 0);
-
-    for (int i = 1; i <= n; i++) {
-        
+ll find_max(vector<ll> buckets) {
+    for (ll i = 9; i >= 0; i--) {
+        if (buckets[i] > 0) {
+            return i;
+        }
     }
+
+    return 0;
+}
+
+vector<ll> digits(ll n) {
+    vector<ll> buckets(10);
+    while (n > 0) {
+        ll digit = n % 10;
+        buckets[digit]++;
+        n /= 10;
+    }
+
+    return buckets;
+}
+
+int main() {
+    ll n; cin >> n; ll temp = n;
+    vector<ll> buckets = digits(n);
+    ll maximum = find_max(buckets);
+
+    ll count = 0;
+    while (n > 0) {
+        ll last_digit = n % 10;
+        if (last_digit > maximum) {
+            n -= maximum;
+            buckets[last_digit]--;
+            buckets[last_digit-maximum]++;
+            maximum = find_max(buckets);
+        } else {
+            n -= maximum;
+            buckets = digits(n);
+            maximum = find_max(buckets);
+        }
+        count++;
+    }
+
+    cout << count;
 
     return 0;
 }
